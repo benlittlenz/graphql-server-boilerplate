5ad9cde812d2a00ec3489d942886550ffd9b4d48
nzbenji <benlittle53@gmail.com>
2019-09-14 11:48:31 +1200

    register err handling

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -11,13 +11,25 @@ export const resolvers: ResolverMap = {
       _,
       { email, password }: GQL.IRegisterOnMutationArguments,
     ) => {
+      const userAlreadyExists = await User.findOne({
+        where: { email },
+        select: ["id"],
+      });
+      if(userAlreadyExists) {
+        return [
+          {
+            path: "email",
+            message: "Already Taken"
+          }
+        ]
+      }
       const hashedPasword = await bcrypt.hash(password, 10);
       const user = User.create({
         email,
         password: hashedPasword,
       });
       await user.save();
-      return true;
+      return null;
     },
   },
 };
