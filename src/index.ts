8bc5c40b6456855e2b0e3f14c851ba21d16e87c2
nzbenji <benlittle53@gmail.com>
2019-09-14 09:21:18 +1200

    yoga and jest configuration

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import * as path from "path";
+import { startServer } from './startServer';
 import "reflect-metadata";
-import { GraphQLServer } from "graphql-yoga";
-import { importSchema } from "graphql-import";
-import { resolvers } from "./resolvers";
 
-import { createTypeormConnection } from "./utils/createTypeormConnect";
-
-export const startServer = async () => {
-  const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
-
-  const server = new GraphQLServer({ typeDefs, resolvers });
-  await createTypeormConnection();
-  await server.start();
-  console.log("Server is running on localhost:4000");
-};
 
 startServer();
--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,11 +1,14 @@
 import { request } from "graphql-request";
-import { host } from "./constants";
 import { User } from "../entity/User";
-import { createTypeormConnection } from "../utils/createTypeormConnect";
+import { startServer } from "../startServer";
 
-beforeAll(async() => {
-  await createTypeormConnection();
-})
+let getHost = () => '';
+
+beforeAll(async () => {
+  const app = await startServer();
+  const { port } = app.address();
+  getHost = () => `http://127.0.0.1:${port}`
+});
 
 const email = "jim@bob.com";
 const password = "123abc";
@@ -17,11 +20,11 @@ const mutation = `
 `;
 
 test("Register User", async () => {
-  const res = await request(host, mutation);
+  const res = await request(getHost(), mutation);
   expect(res).toEqual({ register: true });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
-  const user = users[0]
+  const user = users[0];
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
 });
